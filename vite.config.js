1feef3c3522f7f9002aac4ddc185ca256d557a8c
Milenko Castillo <milenko.castillo@gmail.com>
2023-04-29 21:50:25 -0400

    correct vite config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fcc-random-quote-machine-vite-react/',
+  base: '/fcc-markdown-previewer/',
   plugins: [react()],
 })
